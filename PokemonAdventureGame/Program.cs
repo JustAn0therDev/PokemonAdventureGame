17139556f685855a9db4b1c0522b09d78d18c55e
Ruan Montelo <ruanmontelo@gmail.com>
2020-05-17 02:56:23 -0300

    Removing and changing some comments

--- PokemonAdventureGame/Program.cs
+++ PokemonAdventureGame/Program.cs
@@ -10,16 +10,12 @@ namespace PokemonAdventureGame
     {
         static void Main()
         {
-            //Criar os dois pokemon que entrarao em batalha 
-            //Posterior uso de interfaces?
-            //Criar comandos para atacar (depois trocar de pokemon) e dar itens ao pokemon
-            //Sistema final de batalha, com ataques que sejam super-efetivos, normais, nao efetivos, status ailments, etc...
-
             IPokemon pikachu = PokemonFactory.CreatePokemon<Pikachu>();
 
             IPokemon eevee = PokemonFactory.CreatePokemon<Eevee>();
 
             //TODO: be able to switch first and second pokemon.
+            //The Battle class might receive two lists of IPokemon implementations to manage both player's actions
             Battle battle = new Battle(pikachu, eevee);
 
             battle.PokemonOneMove(Enums.Command.ATTACK);
