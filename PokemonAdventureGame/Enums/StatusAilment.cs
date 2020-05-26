551f7d0fbb794f54c981ce6a4e444c366cf23b88
Ruan Montelo <ruanmontelo@gmail.com>
2020-05-25 23:59:49 -0300

    StatusAilment enumerator create. Will be used in the IPokemon interface and IMove to manage status moves

---
+++ PokemonAdventureGame/Enums/StatusAilment.cs
@@ -0,0 +1,16 @@
+namespace PokemonAdventureGame.Enums 
+{
+    public enum StatusAilment 
+    {
+        ATTACKUP,
+        ATTACKDOWN,
+        DEFENSEUP,
+        DEFENSEDOWN,
+        SPECIALATTACKUP,
+        SPECIALATTACKDOWN,
+        SPECIALDEFENSEUP,
+        SPECIALDEFENSEDOWN,
+        SPEEDUP,
+        SPEEDDOWN
+    }
+}
