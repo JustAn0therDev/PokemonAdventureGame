50b423e8bc8d258b770350b5612d2c93fc312412
Ruan Montelo <ruanmontelo@gmail.com>
2020-07-07 20:59:47 -0300

    Creating tests for items

---
+++ PokemonAdventureGame.Tests/PokemonAdventureGame.Tests/ItemTests.cs
@@ -0,0 +1,23 @@
+﻿using PokemonAdventureGame.Factories;
+using PokemonAdventureGame.Items;
+using System.Linq;
+using Xunit;
+
+namespace PokemonAdventureGame.Tests
+{
+    public class ItemTests
+    {
+        [Fact]
+        public void ShouldReturnAnItem() => Assert.NotNull(ItemFactory.CreateItem<Potion>());
+
+        [Theory]
+        [InlineData(10000)]
+        [InlineData(1)]
+        [InlineData(100)]
+        public void ShouldReturnAListWithNonNullItems(int numberOfItemsToCreate)
+        {
+            var items = ItemFactory.CreateItems<Potion>(numberOfItemsToCreate);
+            Assert.Equal(numberOfItemsToCreate, items.Count());
+        }
+    }
+}
