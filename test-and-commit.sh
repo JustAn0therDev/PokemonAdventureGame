65b84d7817b388fcb284627eb294a26cb280dd5f
Ruan Montelo <ruanmontelo@gmail.com>
2021-11-18 19:41:54 -0300

    Testing pre-commit

--- test-and-commit.sh
+++ test-and-commit.sh
@@ -1,24 +1,25 @@
 OUTPUT=$(dotnet test)
-
-SUCCESS=0
+RESULT=0
 
 while read -r LINE; do
 	echo $LINE
     if [[ $LINE == *"Aprovado"* ]] || [[ $LINE == *"Passed"* ]]
 	then
-		SUCCESS=1
+		RESULT=1
+		break
+	elif [[ $LINE == *"MSB1003"* ]]
+	then
+		RESULT=2
 		break
     fi
-
 done <<< "$OUTPUT"
 
-if [[ $SUCCESS == 1 ]]
+if [[ $RESULT == 1 ]]
 then
-	read -p "Coloque sua mensagem de commit: " COMMIT
-	git add .
-	git commit -m "$COMMIT"
+	echo "Todos os testes passaram. Commit pode ser feito de forma segura."
+elif [[ $RESULT == 2 ]]
+then
+	echo "Este projeto não possui testes."
 else
-	echo "Não é possível realizar o commit pois um ou mais testes não passaram. Por favor, verifique o(s) projeto(s) de teste e tente novamente."
+	echo "Não é possível realizar commit pois alguns dos testes não passaram. Por favor, verifique o(s) projeto(s) de teste e tente novamente."
 fi
-
-echo $SUCCESS
