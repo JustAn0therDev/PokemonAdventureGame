1eed5b80e9ed911ab82e3755b88c4a73eebadc92
Jernej Kavka (JK) <jk@ssw.com.au>
2024-01-07 14:37:53 +1000

    💄 Combat menu with colors

--- PokemonAdventureGame/BattleSystem/ConsoleUI/ConsoleBattleInfo.cs
+++ PokemonAdventureGame/BattleSystem/ConsoleUI/ConsoleBattleInfo.cs
@@ -7,6 +7,10 @@ namespace PokemonAdventureGame.BattleSystem.ConsoleUI
 {
     public static class ConsoleBattleInfo
     {
+        public static readonly ConsoleColor AttackColor = ConsoleColor.White;
+        public static readonly ConsoleColor SpecialAttackColor = ConsoleColor.Yellow;
+        public static readonly ConsoleColor UnavailableMove = ConsoleColor.DarkGray;
+
         public static void ShowAvailableCommandsOnConsole()
         {
             Console.WriteLine($"{(int)Command.ATTACK}: {Command.ATTACK}");
@@ -18,10 +22,26 @@ namespace PokemonAdventureGame.BattleSystem.ConsoleUI
         {
             ConsoleUtils.ShowMessageBetweenEmptyLines("Choose your attack!");
 
+            ConsoleColor previousColor = Console.ForegroundColor;
+
+            int maxMoveNameLength = pokemon.Moves.Max(m => m.GetType().Name.Length);
+            int maxTypeNameLength = pokemon.Moves.Max(m => m.Type.ToString().Length) + 2;
             for (int i = 0; i < pokemon.Moves.Count; i++)
             {
-                Console.WriteLine($"{i}: {pokemon.Moves.ElementAtOrDefault(i).GetType().Name} | PP: {pokemon.Moves.ElementAtOrDefault(i).CurrentPowerPoints}");
+                var move = pokemon.Moves.ElementAtOrDefault(i);
+                string typeName = $"[{move.Type}]".PadRight(maxTypeNameLength);
+                string moveName = move.GetType().Name.PadRight(maxMoveNameLength);
+
+                Console.ForegroundColor = move.Special ? SpecialAttackColor : AttackColor;
+                if (move.CurrentPowerPoints <= 0)
+                {
+                    Console.ForegroundColor = UnavailableMove;
             }
+
+                Console.WriteLine($"{i}: {typeName} {moveName}   {move.CurrentPowerPoints}/{move.PowerPoints}");
+        }
+
+            Console.ForegroundColor = previousColor;
         }
 
         public static void WriteAllAvailableItemsOnConsole(ITrainer player)
